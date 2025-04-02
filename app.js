a24ff0d5a2465b3ff0c1ddfd6f9ccdffe46cd51a
Tylan Drydale <113541994+Tdrydale@users.noreply.github.com>
2025-04-02 14:03:28 -0500

    Part: 9

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dragonsRouter = require('./routes/dragons');
 var gridRouter = require('./routes/grid');
-var randomitemRouter = require('./routes/randomitem');
+var pickRouter = require('./routes/pick');
 
 var app = express();
 
@@ -26,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/dragons', dragonsRouter);
 app.use('/grid', gridRouter);
-app.use('/randomitem', randomitemRouter);
+app.use('/pick', pickRouter);
 
 
 // catch 404 and forward to error handler
