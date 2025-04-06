5b7746cedc567ca54b1c8f28266626ec810a68cf
Tylan Drydale <113541994+Tdrydale@users.noreply.github.com>
2025-04-06 15:49:20 -0500

    Part 7

--- controllers/dragon.js
+++ controllers/dragon.js
@@ -1,23 +1,23 @@
 var Dragon = require('../models/dragon');
 
 exports.dragon_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: Costume list');
+    res.send('NOT IMPLEMENTED: Dragon list');
 };
 
 exports.dragon_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Costume detail: ' + req.params.id);
+    res.send('NOT IMPLEMENTED: Dragon detail: ' + req.params.id);
 };
 
 exports.dragon_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Costume create POST');
+    res.send('NOT IMPLEMENTED: Dragon create POST');
 };
 
 exports.dragon_delete = function(req, res) {
-    res.send('NOT IMPLEMENTED: Costume delete DELETE ' + req.params.id);
+    res.send('NOT IMPLEMENTED: Dragon delete DELETE ' + req.params.id);
 };
 
 exports.dragon_update_put = function(req, res) {
-    res.send('NOT IMPLEMENTED: Costume update PUT' + req.params.id);
+    res.send('NOT IMPLEMENTED: Dragon update PUT' + req.params.id);
 };
 
 exports.dragon_list = async function(req, res) {
@@ -41,4 +41,21 @@ exports.dragon_view_all_Page = async function(req, res) {
         res.send(`{"error": ${err}}`);
     }
 };
+
+exports.dragon_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new Dragon();
+    document.name = req.body.name;
+    document.color = req.body.color;
+    document.age = req.body.age;
+    try{
+        let result = await document.save();
+        res.send(result);
+    }
+    catch(err){
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
+    
     
